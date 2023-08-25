609803b6a1970e09e67e63bb57c2c9b80903260f
楠口恭佑 <kusuguchikyosuke@PS2-10191781-02.local>
2023-08-25 15:03:00 +0900

    first commit

---
+++ calc/calc.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"strconv"
+)
+
+func calc(number1 string, number2 string) (sum int, mul int) {
+	int1, _ := strconv.Atoi(number1)
+	int2, _ := strconv.Atoi(number2)
+	sum = int1 + int2
+	mul = int1 * int2
+	return
+}
